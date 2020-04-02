05e2ddcbfcc98e8d48356bdcfc504fb6bf0303db
Kyler Briscoe <Kyler.Briscoe@gmail.com>
2020-04-02 18:21:02 -0400

    Added creation route, other things

--- users/user.service.js
+++ users/user.service.js
@@ -1,24 +1,89 @@
 ﻿const config = require('config.json');
 const jwt = require('jsonwebtoken');
+const sql = require('mssql/msnodesqlv8');
 
-// users hardcoded for simplicity, store in a db for production applications
-const users = [{ id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' }];
+//Initiallising connection string
+
+async function getUserFromProcedure(procedureName, username, password) {
+    const conn = new sql.ConnectionPool({
+        database:"IAT",
+        server:"localhost",
+        port:52202,
+        driver:"msnodesqlv8",
+        options: {
+          trustedConnection: true
+        }
+    });
+    await conn.connect().then(pool => {
+        return pool.request()
+        .input('pUsername', sql.NVarChar, username)
+        .input('pPassword', sql.NVarChar, password)
+        .output('User', sql.NVarChar)
+        .output('responseMessage', sql.NVarChar)
+        .execute(procedureName)
+    }).then(result => {
+        //console.dir(result)
+        output = result.output
+    }).catch(err => {
+        //...error checking
+        console.log(err)
+    })
+
+    return output
+};
+
+async function addProject(procedureName, title, description, rules) {
+    const conn = new sql.ConnectionPool({
+        database:"IAT",
+        server:"localhost",
+        port:52202,
+        driver:"msnodesqlv8",
+        options: {
+          trustedConnection: true
+        }
+    });
+    await conn.connect().then(pool => {
+        return pool.request()
+        .input('pTitle', sql.NVarChar, title)
+        .input('pDescription', sql.NVarChar, description)
+        .input('pRules', sql.NVarChar, rules)
+        .output('responseMessage', sql.NVarChar)
+        .execute(procedureName)
+    }).then(result => {
+        //console.dir(result)
+        output = result.output
+    }).catch(err => {
+        //...error checking
+        console.log(err)
+    })
+
+    return output
+}
 
 module.exports = {
     authenticate,
+    createProject,
     getAll
 };
 
 async function authenticate({ username, password }) {
-    const user = users.find(u => u.username === username && u.password === password);
-    if (user) {
-        const token = jwt.sign({ sub: user.id }, config.secret);
-        const { password, ...userWithoutPassword } = user;
-        return {
-            ...userWithoutPassword,
-            token
-        };
+    console.log("Attempting login with " + username)
+    const user = await Promise.resolve(getUserFromProcedure('Login', username, password));
+    if (user.User !== null) {
+        userValues = JSON.parse(user.User)
+        var Fname = userValues[0].FirstName
+        if (userValues[0].Admin) {permissions = ['Administrator']}
+        else{permissions = []}
+        const token = jwt.sign({ sub: userValues[0].UserID, iss:'IAT', permissions:permissions }, config.secret);
+        return { Fname, token };
     }
+}
+
+async function createProject({ title, description, rules, labels }) {
+    console.log("Attempting create project: " + title)
+    const result = await Promise.resolve(addProject('CreateProject', title, description, rules));
+    console.log(result.responseMessage)
+    return result.responseMessage
 }
 
 async function getAll() {
--- users/users.controller.js
+++ users/users.controller.js
@@ -4,6 +4,7 @@ const userService = require('./user.service');
 
 // routes
 router.post('/authenticate', authenticate);
+router.post('/createProject', createProject);
 router.get('/', getAll);
 
 module.exports = router;
@@ -11,6 +12,13 @@ module.exports = router;
 function authenticate(req, res, next) {
     userService.authenticate(req.body)
         .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
+        .catch(err => next(err));
+}
+
+function createProject(req, res, next) {
+    console.log(req)
+    userService.createProject(req.body)
+        .then(user => user ? res.json(user) : res.status(400).json({ message: 'Failed to add Project' }))
         .catch(err => next(err));
 }
 
